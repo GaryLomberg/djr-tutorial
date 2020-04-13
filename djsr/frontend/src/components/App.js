1e3d5c1b27adb88f28d57fd8d65b17d3f8b2a198
Gary Lomberg <g.lmbrg@@gmail.com>
2020-04-13 11:06:07 +0200

    render first component

---
+++ djsr/frontend/src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return(
+            <h1>Finally time for app development</h1>
+        )
+    }
+}
